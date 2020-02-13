dcde9477bab6f62fb1ee7ea3ad451b5162992135
Dellan Muchengapadare <37017264+mactunechy@users.noreply.github.com>
2020-02-13 13:42:03 +0200

    Update employees.js

---
+++ src/server/routes/employees.js
@@ -0,0 +1,34 @@
+/*
+* User router
+*
+*/
+
+//Dependencies
+const controller = require ('../controllers/employees.js');
+const express = require ('express');
+const validateEmail = require ('../middleware/validateEmail');
+const auth = require ('../middleware/auth');
+const router = express.Router ();
+const {upload} = require ('../lib/fileUploader');
+
+router.post (
+  '/accounts/create',
+  upload.single ('file'),
+  validateEmail,
+  controller.createUser
+);
+router.post ('/accounts/create/confirm', controller.confirmAccountCreation);
+router.put (
+  '/accounts/update',
+  auth (true, 'green'),
+  upload.single ('file'),
+  controller.updateUser
+);
+router.delete ('/accounts/delete/:id', controller.deleteUser);
+router.get ('/accounts/me/:id', auth (true, 'green'), controller.getEmployee);
+router.get ('/accounts/all', auth (true, 'green'), controller.getEmployees); //@TODO add auth
+router.post ('/accounts/login', controller.loginUser);
+router.put ('/accounts/renew/token', auth, controller.renewToken);
+
+//Exporting the router
+module.exports = router;
