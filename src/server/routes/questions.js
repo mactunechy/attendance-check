d5f4828dde2dcc7e6efd7a866b371088636e4445
Dellan Muchengapadare <37017264+mactunechy@users.noreply.github.com>
2020-02-13 13:36:48 +0200

    Update questions.js

--- src/server/routes/questions.js
+++ src/server/routes/questions.js
@@ -4,32 +4,27 @@
 */
 
 //Dependencies
-const controller = require ('../controllers/questions.js');
+const controller = require ('../controllers/attendance');
 const express = require ('express');
 const auth = require ('../middleware/auth');
 const router = express.Router ();
 const {upload} = require ('../lib/fileUploader');
 
 router.post (
-  '/questions/create',
-  auth (true, 'green'),
-  upload.single ('file'),
+  '/attendance/create',
   controller.create
 );
-router.post ('/question/mark/:id', auth (true, 'green'), controller.mark);
+router.post ('/question/mark/:id', controller.mark);
 router.put (
-  '/questions/update/:id',
-  auth (true, 'green'),
-  upload.single ('file'),
-  controller.updateQuestion
+  '/attendance/update/:id',
+  controller.updateAttendance
 );
 router.delete (
-  '/questions/delete/:id',
-  auth (true, 'green'),
-  controller.deleteQuestion
+  '/attendance/delete/:id',
+  controller.deleteAttendance
 );
-router.get ('/question/:id', auth (true, 'green'), controller.getQuestion);
-router.get ('/questions/all', auth (true, 'green'), controller.getQuestions); //@TODO add auth
+router.get ('/attendance/:id', controller.getAttendance)
+router.get ('/attendance/all', controller.getAttendances; //@TODO add auth
 
 //Exporting the router
 module.exports = router;
