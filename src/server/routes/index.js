61ce22fa2b816d09fd0dbf29bdc028788d41a8d8
Dellan Muchengapadare <37017264+mactunechy@users.noreply.github.com>
2020-02-13 13:25:47 +0200

    Update index.js

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,20 +6,13 @@
 const users = require ('./users');
 const ping = require ('./ping');
 const logger = require ('../middleware/logger');
-const questions = require ('./questions');
-const notes = require ('./notes');
-const exams = require ('./exams');
-const tokens = require ('./tokens');
-const topics = require ('./topics');
+const attendance = require ('./attendance');
+
 
 //root router function
 const rootRouter = function (app) {
   app.use ('/api', logger, users);
-  app.use ('/api', logger, questions);
-  app.use ('/api', logger, notes);
-  app.use ('/api', logger, exams);
-  app.use ('/api', logger, tokens);
-  app.use ('/api', logger, topics);
+  app.use ('/api', logger, attendance);
   app.use ('/api', ping);
 };
 
